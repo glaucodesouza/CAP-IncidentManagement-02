6cf95927d06d7f0ef4cb1b7d66408b25b9e5bda8
Glauco Kubrusly <gkubrusly@deloitte.com>
2024-02-25 19:54:07 +0000

    Add Test Cases

--- srv/processor-service.js
+++ srv/processor-service.js
@@ -24,7 +24,7 @@ class ProcessorService extends cds.ApplicationService {
   async onUpdate (req) {
     const { status_code } = await SELECT.one(req.subject, i => i.status_code).where({ID: req.data.ID})
     if (status_code === 'C')
-      return req.reject(`Não pode modificar um incidente fechado`)
+      return req.reject(`Can't modify a closed incident`)
   }
 }
 module.exports = ProcessorService
